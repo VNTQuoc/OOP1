0778a78397b32b9d5db7c3e0effa3be5e43e22e5
Tran Quoc <voquoc0103@gmail.com>
2022-05-25 21:33:08 +0700

    OOP Bai tap 1

---
+++ OOP_Bai1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace BT
+{
+    class toado
+    {
+        int x, y;
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        
+        public void output()
+        {
+            Console.WriteLine("{0}({1},{2})", this.name, this.x, this.y);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            toado p = new toado();
+            Console.WriteLine("Please type the Name: ");
+            p.Name = Console.ReadLine();
+            Console.WriteLine("Please type the number for x: ");
+            p.X = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please type the number for y: ");
+            p.Y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Result: ");
+            p.output();
+
+            
+        }
+    }
+    
+}
